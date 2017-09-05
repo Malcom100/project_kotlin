d4d4ce88962fda6e5a73948370fc68a156d8cced
Malcom <gtshilombowanticale@adneom.com>
2017-09-05 15:29:12 +0200

    add parcelable to class and delete primary constructor

--- app/src/main/java/adneom/project_kotlin/models/User.kt
+++ app/src/main/java/adneom/project_kotlin/models/User.kt
@@ -1,8 +1,10 @@
 package adneom.project_kotlin.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 
-class User (mName : String, mId : Int, mUsername : String, mEmail : String, mPhone : String, mWebSite : String, mAddress: Address, mCompany : Company){
+class User  : Parcelable {
     @Expose
     var name : String? = null
     get
@@ -36,7 +38,7 @@ class User (mName : String, mId : Int, mUsername : String, mEmail : String, mPho
     get
     set
 
-    init {
+    /*init {
         name = mName
         id = mId
         username = mUsername
@@ -45,7 +47,40 @@ class User (mName : String, mId : Int, mUsername : String, mEmail : String, mPho
         website = mWebSite
         address = mAddress
         company = mCompany
+    }*/
+
+    constructor(spurce: Parcel) : super() {
+        name = spurce.readString()
+        id = spurce.readInt()
+        username = spurce.readString()
+        email = spurce.readString()
+        phone = spurce.readString()
+        website = spurce.readString()
     }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(source: Parcel): User {
+            return User(source)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest!!.writeString(name)
+        dest!!.writeInt(id!!)
+        dest!!.writeString(username)
+        dest!!.writeString(email)
+        dest!!.writeString(phone)
+        dest!!.writeString(website)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
 
     override fun toString() : String {
         return "{".plus(id).plus(" ").plus(name).plus(" ").plus(username).plus(" ").plus(email).plus(" ").plus(phone).plus(" ").plus(website).plus(" - ").plus(address.toString()).plus(" - ").plus(company.toString()).plus("}")
