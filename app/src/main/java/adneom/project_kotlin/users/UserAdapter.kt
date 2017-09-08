b1affd1b633982c0d3672fa160ddaefe069320a5
Malcom <gtshilombowanticale@adneom.com>
2017-09-08 17:06:22 +0200

    add color to list

--- app/src/main/java/adneom/project_kotlin/users/UserAdapter.kt
+++ app/src/main/java/adneom/project_kotlin/users/UserAdapter.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.users_list_row.*
 
@@ -17,6 +18,7 @@ class UserAdapter(cxt : Context, items : ArrayList<User>) : RecyclerView.Adapter
         list = items
         context = cxt
     }
+    private var colors_tmp : ArrayList<Int> = ArrayList<Int>()
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): UserViewHolder {
         var itemView : View = LayoutInflater.from(context).inflate(R.layout.users_list_row,parent,false)
@@ -29,6 +31,37 @@ class UserAdapter(cxt : Context, items : ArrayList<User>) : RecyclerView.Adapter
         holder!!.userName.setText(user.username)
         holder!!.email.setText(user.email)
         holder!!.id.setText(user.id.toString())
+        holder!!.primaryContent.setBackgroundColor(choiceColor())
+
+    }
+
+    private fun choiceColor() : Int{
+        var color_chosen : Int = R.color.list_color_one
+        if(colors_tmp.size == 0){
+            color_chosen  = context.resources.getColor(R.color.list_color_one)
+            colors_tmp.add(1)
+        }else{
+            when(colors_tmp.size) {
+                1 ->
+                {
+                    color_chosen  = context.resources.getColor(R.color.list_color_two)
+                    colors_tmp.add(2)
+                }
+
+                2 ->
+                {
+                    color_chosen  = context.resources.getColor(R.color.list_color_three)
+                    colors_tmp.add(3)
+                }
+
+                3 ->
+                {
+                    color_chosen  = context.resources.getColor(R.color.list_color_one)
+                    colors_tmp.clear()
+                }
+            }
+        }
+        return color_chosen
     }
 
     override fun getItemCount(): Int {
@@ -41,11 +74,13 @@ class UserAdapter(cxt : Context, items : ArrayList<User>) : RecyclerView.Adapter
         lateinit var userName : TextView
         lateinit var email : TextView
         lateinit var id : TextView
+        lateinit var primaryContent : LinearLayout
         init {
             name = view.findViewById(R.id.name) as TextView
             userName = view.findViewById(R.id.username) as TextView
             email = view.findViewById(R.id.email) as TextView
             id = view.findViewById(R.id.id) as TextView
+            primaryContent = view.findViewById(R.id.primary_content) as LinearLayout
         }
     }
 }
